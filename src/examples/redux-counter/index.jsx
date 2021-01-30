5bd0684ed9014e712b33b61c955eaa7e42604820
Sabin Marcu <sabinmarcu@gmail.com>
2021-01-30 12:10:03 +0200

    nested counters with context

--- src/examples/redux-counter/index.jsx
+++ src/examples/redux-counter/index.jsx
@@ -2,53 +2,91 @@ import {
   Button, ButtonGroup, Card, CardActions, CardHeader,
 } from '@material-ui/core';
 import { useCallback } from 'react';
-
-import { Provider, useSelector, useDispatch } from 'react-redux';
 import { valueSelector, actions } from './reducer';
-import { store } from './store';
+import { createCounter } from './store';
 
-const CounterButtons = () => {
-  const dispatch = useDispatch();
-  const increment = useCallback(
-    (value) => dispatch(actions.increment(value)),
-    [dispatch],
+const createCounterRender = (name) => {
+  const {
+    Provider,
+    Context,
+    useSelector,
+    useDispatch,
+  } = createCounter();
+
+  const CounterButtons = () => {
+    const dispatch = useDispatch();
+    const increment = useCallback(
+      (value) => dispatch(actions.increment(value)),
+      [dispatch],
+    );
+    const decrement = useCallback(
+      (value) => dispatch(actions.decrement(value)),
+      [dispatch],
+    );
+    const reset = useCallback(
+      () => dispatch(actions.reset()),
+      [dispatch],
+    );
+    return (
+      <ButtonGroup>
+        <Button onClick={() => decrement(5)}>-5</Button>
+        <Button onClick={() => decrement(1)}>-1</Button>
+        <Button onClick={() => reset()}>Reset</Button>
+        <Button onClick={() => increment(1)}>+1</Button>
+        <Button onClick={() => increment(5)}>+5</Button>
+      </ButtonGroup>
+    );
+  };
+
+  const Counter = ({ children }) => {
+    const counter = useSelector(valueSelector);
+    return (
+      <>
+        <Card>
+          <CardHeader
+            title={counter}
+            subheader={`${name}: Counter Value`}
+          />
+          <CardActions>
+            <CounterButtons />
+          </CardActions>
+        </Card>
+        {children}
+      </>
+    );
+  };
+
+  const Component = (props) => (
+    <Provider>
+      <Counter {...props} />
+    </Provider>
   );
-  const decrement = useCallback(
-    (value) => dispatch(actions.decrement(value)),
-    [dispatch],
-  );
-  const reset = useCallback(
-    () => dispatch(actions.reset()),
-    [dispatch],
-  );
-  return (
-    <ButtonGroup>
-      <Button onClick={() => decrement(5)}>-5</Button>
-      <Button onClick={() => decrement(1)}>-1</Button>
-      <Button onClick={() => reset()}>Reset</Button>
-      <Button onClick={() => increment(1)}>+1</Button>
-      <Button onClick={() => increment(5)}>+5</Button>
-    </ButtonGroup>
-  );
+
+  return { Component, Context };
 };
 
-const Counter = () => {
-  const counter = useSelector(valueSelector);
-  return (
-    <Card>
-      <CardHeader
-        title={counter}
-        subheader="Counter Value"
-      />
-      <CardActions>
-        <CounterButtons />
-      </CardActions>
-    </Card>
-  );
-};
+const Counter1 = createCounterRender('First');
+const Counter2 = createCounterRender('Second');
 
 export default () => (
-  <Provider store={store}>
-    <Counter />
-  </Provider>
+  <Counter1.Component>
+    <Counter2.Component>
+      <Counter1.Context.Consumer>
+        {({ store: { dispatch } }) => (
+          <Card>
+            <CardHeader title="Modify First Counter:" />
+            <CardActions>
+              <ButtonGroup>
+                <Button onClick={() => dispatch(actions.decrement(5))}>-5</Button>
+                <Button onClick={() => dispatch(actions.decrement(1))}>-1</Button>
+                <Button onClick={() => dispatch(actions.reset())}>Reset</Button>
+                <Button onClick={() => dispatch(actions.increment(1))}>+1</Button>
+                <Button onClick={() => dispatch(actions.increment(5))}>+5</Button>
+              </ButtonGroup>
+            </CardActions>
+          </Card>
+        )}
+      </Counter1.Context.Consumer>
+    </Counter2.Component>
+  </Counter1.Component>
 );
---
+++ src/examples/redux-counter/store.jsx
@@ -0,0 +1,33 @@
+import {
+  createStore,
+} from 'redux';
+import {
+  Provider as ReduxProvider,
+  createDispatchHook,
+  createSelectorHook,
+} from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { createContext } from 'react';
+import { reducer } from './reducer';
+
+export const createCounter = () => {
+  const Context = createContext();
+  const store = createStore(
+    reducer,
+    composeWithDevTools(),
+  );
+  const Provider = ({ children }) => (
+    <ReduxProvider store={store} context={Context}>
+      {children}
+    </ReduxProvider>
+  );
+  const useDispatch = createDispatchHook(Context);
+  const useSelector = createSelectorHook(Context);
+
+  return {
+    Provider,
+    Context,
+    useDispatch,
+    useSelector,
+  };
+};
